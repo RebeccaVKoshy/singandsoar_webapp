8ed554c945fc28e0cdcc8df458fd2042179fa0cd
topinsn <olaoluwa54@gmail.com>
2023-01-17 09:02:13 -0500

    laying background

---
+++ footer.php
@@ -0,0 +1,24 @@
+
+
+
+<div class="footer d-flex justify-content-center">
+    <p>&copy Sing and Soar <?php echo date('Y');?></p>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php require_once "header.php";?>
+
+<body class="container-fluid">
+    <div class="row">
+        <div class="col-md-12 bg-warning">
+            <ul class="nav d-flex justify-content-end">
+                <li class="nav-item ps-5">Home</li>
+                <li class="nav-item ps-5">Contact</li>
+                <li class="nav-item ps-5 pe-3">About</li>
+            </ul>
+        </div>
+    </div>
+
+    
+<?php
+    require_once "footer.php";
+?>
